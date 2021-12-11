501de2d2424c7013794227472cdf4bc3b34e7172
NicolaDemartini <91873902+NicolaDemartini@users.noreply.github.com>
2021-12-11 21:19:36 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+
+    int d;
+    cin >> d;
+    float a[d];
+
+    for(int i=0; i<d ; i++){
+        cin >> a[i];
+    }
+        maggiore(d,a);
+    cout << maggiore(d,a) << endl;
+
+    return 0;
 }
